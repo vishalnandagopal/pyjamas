7ccecdf2e16d3d65eb36e80c3e16366b6f3dbbd1
Sreenath <91029380+StormPredator-Sr@users.noreply.github.com>
2023-01-15 21:44:47 +0530

    Added comments

--- pyjamas/__main__.py
+++ pyjamas/__main__.py
@@ -1,8 +1,7 @@
 from getpass import getpass
-
 from .database import store_in_database as store_in_database
 
-
+""" Take username and password input from user and store them in database."""
 entered_username = input("Enter your username: ")
 plain_text_password = getpass("Enter password: ")
 store_in_database(
